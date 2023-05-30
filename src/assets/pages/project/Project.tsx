5e2d13e1c5d69139e45f27fe88cf8ee4a254f4e7
AndreaRospi <andr3z45@gmail.com>
2023-05-30 12:15:13 +0200

    inizio grafica page projct

--- src/assets/pages/project/Project.tsx
+++ src/assets/pages/project/Project.tsx
@@ -1,8 +1,29 @@
 const Project = () => {
   return (
-    <div>
-      <h1>Project page</h1>
-      content here...
+    <div className="flex w-full h-full bg-red-500">
+      <div className="w-1/4 bg-lime-400">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi,
+          officia sapiente libero delectus cumque temporibus. Tempore tenetur
+          perspiciatis est illum quae, odio officiis ut error repellat.
+          Consectetur recusandae dignissimos beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iste, dolorem accusantium saepe, officiis harum nesciunt praesentium unde id maiores aperiam quod magni dolore nobis porro, necessitatibus doloremque quidem rerum?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti error fugiat quisquam asperiores sed beatae quae aliquam harum vero repellendus incidunt quibusdam atque quos accusantium iure vitae eius corporis.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quidem, natus vel, nihil nesciunt saepe recusandae quas aut itaque, totam ex magni! Ullam culpa, corrupti alias officiis debitis doloribus repellendus.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, reprehenderit sapiente. Similique voluptatem corrupti cumque fugit incidunt dolorum voluptates blanditiis eos. Enim hic recusandae quos expedita veniam qui optio consequatur.
+        </p>
+      </div>
+
+      <div className="w-3/4 bg-violet-900 px-12">
+        <h2 className="text-2xl font-medium text-center py-7">
+          Selezionare commessa
+        </h2>
+        <div className="grid grid-cols-2 gap-8">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            prova spazio 1
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
