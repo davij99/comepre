4c15048916113bd767d7b16b57e2dfaf11ca4a68
Davide Cugliari <67780155+davij99@users.noreply.github.com>
2023-06-05 12:44:14 +0200

    projects funzionanti??

---
+++ src/model/projectI.ts
@@ -0,0 +1,14 @@
+export interface ProjectI {
+  assigned_to?: string[] | null;
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  description: string;
+  hours: number;
+  id: string;
+  material_used?: string[] | null;
+  material_used_qty: number;
+  project_name: string;
+  status: string;
+  updated: string;
+}
