5193757027803838779d502473339822b4e3f115
AndreaRospi <andr3z45@gmail.com>
2023-06-13 12:06:26 +0200

    custom material completata

--- src/assets/pages/materials/CustomMaterials.tsx
+++ src/assets/pages/materials/CustomMaterials.tsx
@@ -1,6 +1,61 @@
+import { useNavigate } from "react-router-dom";
 import Aside from "../../shared/components/core/Aside";
+import Button from "../../shared/components/core/Button";
+import { getMaterial } from "../../../services/materials/materials.api";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const CustomMaterials = () => {
+  const [productId, setProductId] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [qty, setQty] = useState<number | "">("");
+  const navigate = useNavigate();
+  function handleProductIdChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setProductId(value);
+
+    if (value === "") {
+      // Se il campo del codice prodotto è vuoto, resetta la descrizione del prodotto
+      setProductDescription("");
+    } else {
+      // Chiamata API quando l'utente ha finito di scrivere nell'input
+      getMaterial(value)
+        .then((res) => {
+          const nameMaterial = res.material_name;
+          const categorieMaterial = res.categories;
+          const um = res.um;
+          const description = `${nameMaterial}\n${categorieMaterial}\n${um}`;
+          console.log(res);
+          setProductDescription(description); // Imposta la descrizione del prodotto
+        })
+        .catch((error) => {
+          console.log(error);
+          setProductDescription("Codice prodotto errato");
+        });
+    }
+  }
+
+  function sendData(e: FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.log("ID inserito:", productId);
+    console.log("Qty:", qty);
+    const selectedProject = JSON.parse(
+      localStorage.getItem("selectedProject") || "{}"
+    );
+    const projectId = selectedProject.id;
+    setProductId("");
+    setProductDescription("");
+    setQty("");
+  }
+
+  function goBack() {
+    const selectedProject = JSON.parse(
+      localStorage.getItem("selectedProject") || "{}"
+    );
+    const projectId = selectedProject.id;
+    localStorage.removeItem("selectedAction");
+    navigate(`/activity/${projectId}`);
+  }
+
   return (
     <div className="flex w-full">
       <Aside />
@@ -9,31 +64,43 @@ const CustomMaterials = () => {
           Scarico Materiali :
         </h2>
         <form className="w-2/4  m-auto" action="">
-        <div className=" flex items-center justify-between">
-          <label className="pr-5 text-xl">Inserisci codice prodotto:</label>
-          <input
-            className="border border-[#172842] py-4 rounded-2xl text-center"
-            type="text"
-          />
-        </div>
-        <div className=" my-10 flex flex-col justify-center ">
-          <label className="pr-5 text-xl">Descrizione prodotto:</label>
-          <textarea
-            className="w-full h-36 resize-none border border-[#172842] p-4 rounded-2xl "
-            disabled
-            value="prova"
-          />
-        </div>
-        <div className=" flex justify-between items-center">
-          <label className="pr-5 text-xl">QTY:</label>
-          <input
-            className="border border-[#172842] py-4 rounded-2xl text-center"
-            type="text"
-          />
-        </div>
-
-
+          <div className=" flex items-center justify-between">
+            <label className="pr-5 text-xl">Inserisci codice prodotto:</label>
+            <input
+              className="border border-[#172842] py-4 rounded-2xl text-center"
+              type="text"
+              value={productId}
+              onChange={handleProductIdChange}
+            />
+          </div>
+          <div className=" my-10 flex flex-col justify-center ">
+            <label className="pr-5 text-xl">Descrizione prodotto:</label>
+            <textarea
+              className="w-full h-36 resize-none border border-[#172842] p-4 rounded-2xl "
+              disabled
+              value={productDescription}
+            />
+          </div>
+          <div className=" flex justify-between items-center">
+            <label className="pr-5 text-xl">QTY:</label>
+            <input
+              className="border border-[#172842] py-4 rounded-2xl text-center"
+              type="number"
+              value={qty === "" ? "" : String(qty)}
+              onChange={(e) => setQty(Number(e.target.value))}
+            />
+          </div>
         </form>
+        <div className="flex justify-around mt-9">
+          <Button handleClick={() => goBack()} />
+          <button
+            className="bg-red-500 px-24 py-6 text-white rounded-xl shadow-2xl "
+            type="submit"
+            onClick={sendData}
+          >
+            Scarica Materiali
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/services/materials/materials.api.ts
+++ src/services/materials/materials.api.ts
@@ -1,5 +1,6 @@
 import { pb } from "../../pocketbase.ts";
 import { MaterialsByProject } from "../../model/materialsByProject.ts";
+import { Material } from "../../model/material.ts";
 
 export function get() {
   return pb.collection("projects_materials").getList<MaterialsByProject[]>();
@@ -7,4 +8,8 @@ export function get() {
 
 export function update(id: string, material_used_qty: number) {
   return pb.collection("projects").update(id, { material_used_qty });
+}
+
+export function getMaterial(id:string)  {
+  return pb.collection("materials").getOne<Material>(id);
 }
